c8e6eddc099e27565bf14a66cbb2dacc1d844e1a
Sudarshan Varadarajan <Sudarshan.Varadarajan16@gmail.com>
2026-04-28 20:43:40 +0200

    app updates

--- app/main.py
+++ app/main.py
@@ -321,7 +321,7 @@ if menu_selection == "🏠 Home":
     fav_head_col, fav_add_col = st.columns([9, 1], vertical_alignment="center")
     with fav_head_col: st.subheader("⭐ Your Favorite Dashboards")
     with fav_add_col:
-            with st.popover("➕ Add", use_container_width=True):
+            with st.popover("➕ Add", width="stretch"):
                 home_search = st.text_input("🔎 Search App Name", key="home_search")
                 df_home_display = prep_display_df(df_original, home_search)
                 cols_to_keep = ['favorite', 'app_id', 'platform', 'environment', 'app_name', 'space_name', 'app_link']
@@ -330,7 +330,7 @@ if menu_selection == "🏠 Home":
                 
                 st.data_editor(
                     df_home_display, column_config=column_config_settings, hide_index=True,
-                    use_container_width=True, disabled=cols_to_disable, key="home_editor",
+                    width="stretch", disabled=cols_to_disable, key="home_editor",
                     on_change=process_updates, args=("home_editor", df_home_display)
                 )
 
